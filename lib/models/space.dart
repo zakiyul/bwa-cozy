0d1593b700112441533358ac313dd12e91feb8d5
zakiyul <zakiyul.py@gmail.com>
2023-01-20 16:37:29 +0800

    add provider

--- lib/models/space.dart
+++ lib/models/space.dart
@@ -1,18 +1,49 @@
 class Space {
-  int id;
-  String name;
-  int price;
-  String city;
-  String country;
-  String imgUrl;
-  int rating;
+  late int id;
+  late String name;
+  late int price;
+  late String city;
+  late String country;
+  late String imgUrl;
+  late int rating;
+  late String address;
+  late String phone;
+  late String mapUrl;
+  late List photos;
+  late int numberOfKitchens;
+  late int numberOfBedrooms;
+  late int numberOfCupboards;
 
-  Space(
-      {required this.id,
-      required this.name,
-      required this.price,
-      required this.city,
-      required this.country,
-      required this.imgUrl,
-      required this.rating});
+  Space({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.city,
+    required this.country,
+    required this.imgUrl,
+    required this.rating,
+    required this.address,
+    required this.phone,
+    required this.mapUrl,
+    required this.photos,
+    required this.numberOfKitchens,
+    required this.numberOfBedrooms,
+    required this.numberOfCupboards,
+  });
+
+  Space.fromJson(json) {
+    id = json['id'];
+    name = json['name'];
+    price = json['price'];
+    city = json['city'];
+    country = json['country'];
+    imgUrl = json['image_url'];
+    rating = json['rating'];
+    address = json['address'];
+    phone = json['phone'];
+    mapUrl = json['map_url'];
+    numberOfBedrooms = json['number_of_bedrooms'];
+    numberOfCupboards = json['number_of_cupboards'];
+    numberOfKitchens = json['number_of_kitchens'];
+  }
 }
---
+++ lib/providers/space_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SpaceProvider extends ChangeNotifier {
+  getRecommendedSpaces() async {
+    // var result = await http.get()
+  }
+}
